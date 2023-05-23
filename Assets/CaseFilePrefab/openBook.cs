075cd3f5aad666d83ce392ec48489ee0fdcfecc4
Nicole <nicolewiedmaier97@gmail.com>
2023-05-23 17:33:53 +0200

    Various Fixes
    -Character speed to 3
    -Journal Canvas resized to show up in world
    -Notifications rotated correctly

--- Assets/CaseFilePrefab/openBook.cs
+++ Assets/CaseFilePrefab/openBook.cs
@@ -6,12 +6,19 @@ public class openBook : MonoBehaviour
 {
     public GameObject Cover;
     public HingeJoint myHinge;
+    Journal journal;
     
     void Start()
     {
         var myHinge = Cover.GetComponent<HingeJoint>();
         myHinge.useMotor = true;
 
+    }
+
+    void Awake()
+    {
+        CloseBook();
+        journal = GetComponentInChildren<Journal>();
     }
 
     // Update is called once per frame
@@ -21,6 +28,7 @@ public class openBook : MonoBehaviour
         motor.targetVelocity = 200;
         myHinge.motor = motor;
         Debug.Log("motor should be true");
+        StartCoroutine(ActivateJournalAfterSeconds(3));
     }
     public void CloseBook()
     {
@@ -30,4 +38,9 @@ public class openBook : MonoBehaviour
         Debug.Log("motor should be false");
     }
 
+    public IEnumerator ActivateJournalAfterSeconds(int time)
+    {
+        yield return new WaitForSeconds(time);
+
+    }
 }
